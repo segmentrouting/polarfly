9debe406133ad37fd84a057711d9bd2ae62104e5
brmcdoug <brmcdoug@cisco.com>
2025-02-23 11:53:27 -0800

    typo

--- radix-8-xrd/util/cleanup-lower.sh
+++ radix-8-xrd/util/cleanup-lower.sh
@@ -4,3 +4,6 @@
 
 sudo clab destroy -t polarfly-lower.yml
 sudo rm -rf clab-polarfly-radix8
+
+docker ps -a | grep Removal | awk '{print $1}' | xargs -n1 docker rm 
+docker ps -a | grep Exited | awk '{print $1}' | xargs -n1 docker rm 
