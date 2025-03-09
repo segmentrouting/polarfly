4981ede0a678a2aec9cdeb84be0cf4ad7c926cbc
brmcdoug <brmcdoug@cisco.com>
2025-03-08 18:05:56 -0800

    trex

--- radix-8-xrd/util/cleanup-lower.sh
+++ radix-8-xrd/util/cleanup-lower.sh
@@ -1,10 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
 # Cleanup the lower topology
 
 sudo clab tools vxlan delete -p clab
-
-sudo clab destroy -t ../polarfly-lower.yml -c
 
 docker ps -a | grep Removal | awk '{print $1}' | xargs -n1 docker rm 
 docker ps -a | grep Exited | awk '{print $1}' | xargs -n1 docker rm 
--- radix-8-xrd/util/cleanup-upper.sh
+++ radix-8-xrd/util/cleanup-upper.sh
@@ -1,9 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
 # Cleanup the upper topology
 
 sudo clab tools vxlan delete -p clab
-sudo clab destroy -t ../polarfly-upper.yml -c
 
 docker ps -a | grep Removal | awk '{print $1}' | xargs -n1 docker rm 
 docker ps -a | grep Exited | awk '{print $1}' | xargs -n1 docker rm 
