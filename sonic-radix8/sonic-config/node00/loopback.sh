2f3876fe0812f5bf29a39098ce3536c09ddb594e
brmcdoug <brmcdoug@cisco.com>
2025-05-04 19:31:38 -0700

    fewer hosts

--- sonic-radix8/sonic-config/node00/loopback.sh
+++ sonic-radix8/sonic-config/node00/loopback.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo config interface ip add Loopback0 10.0.0.200/32
-sudo config interface ip add Loopback0 fc00:0:1200::1/128
+sudo config interface ip add Loopback0 10.0.0.0/32
+sudo config interface ip add Loopback0 fc00:0:1000::1/128
--- sonic-radix8/sonic-config/node01/loopback.sh
+++ sonic-radix8/sonic-config/node01/loopback.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo config interface ip add Loopback0 10.0.0.201/32
-sudo config interface ip add Loopback0 fc00:0:1201::1/128
+sudo config interface ip add Loopback0 10.0.0.1/32
+sudo config interface ip add Loopback0 fc00:0:1001::1/128
